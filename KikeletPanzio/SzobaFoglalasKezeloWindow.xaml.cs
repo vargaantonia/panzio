d84da84194c29922cc1936070e067b9a32da29c0
vargaantonia <145546330+vargaantonia@users.noreply.github.com>
2024-05-30 13:31:27 +0200

    Add files via upload

---
+++ KikeletPanzio/SzobaFoglalasKezeloWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace KikeletPanzio
+{
+    /// <summary>
+    /// Interaction logic for SzobaFoglalasKezeloWindow.xaml
+    /// </summary>
+    public partial class SzobaFoglalasKezeloWindow : Window
+    {
+        public SzobaFoglalasKezeloWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegisztral_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
